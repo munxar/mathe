93d2d4232be2ffe074761440179a46fd308fd356
Sascha Aeppli <sascha.aeppli@liip.ch>
2025-01-27 17:14:38 +0100

    add score and difficulty

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,9 @@
 	const init = () => {
 		answer = '';
 		isCorrect = undefined;
-		a = Math.floor(Math.random() * 10);
-		b = Math.floor(Math.random() * 10);
+		const range = (Math.ceil(score / 10) || 1) * 10;
+		a = Math.floor(Math.random() * range);
+		b = Math.floor(Math.random() * range);
 	};
 
 	init();
@@ -46,7 +47,7 @@
 	<div class="p-4">
 		{#if gameOver}
 			<div class="text-xl">Game Over</div>
-			<div>Du hast {score} Punkte erreicht.</div>
+			<div>Du hast {score} 🟡 erreicht.</div>
 			<button class="px-4 py-2 cursor-pointer rounded bg-slate-400" onclick={reset}>nochmal?</button
 			>
 		{:else}
@@ -55,26 +56,25 @@
 					{#each { length: lifes }}♥{/each}
 				</div>
 				<div>
-					🟡 {score}
+					{score} 🟡
 				</div>
 			</div>
 			<form onsubmit={onAnswer} class="flex justify-center">
-				<div class="text-3xl mt-8">
-					{a} + {b} =
-					<input
-						bind:value={answer}
-						type="text"
-						step={1}
-						placeholder=""
-						class="border-b w-9 text-right px-1"
-					/>
+				<div>
+					<div class="text-3xl mt-8">
+						{a} + {b} =
+						<input
+							bind:value={answer}
+							type="text"
+							step={1}
+							placeholder=""
+							class="border-b w-16 text-right px-1"
+						/>
+					</div>
+					{#if isCorrect === false}
+						<div class="mt-2">Das ist leider falsch.</div>
+					{/if}
 				</div>
-				{#if isCorrect === true}
-					<div>Das ist richtig.</div>
-				{/if}
-				{#if isCorrect === false}
-					<div>Das ist leider falsch.</div>
-				{/if}
 			</form>
 		{/if}
 	</div>
